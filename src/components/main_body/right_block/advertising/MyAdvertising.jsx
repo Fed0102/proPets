23bb92c3661b3d2bfe0445d3116ec685ac759504
Fed0102 <127963492+Fed0102@users.noreply.github.com>
2023-03-25 22:39:25 +0300

    change start_page

--- src/components/main_body/right_block/advertising/MyAdvertising.jsx
+++ src/components/main_body/right_block/advertising/MyAdvertising.jsx
@@ -6,7 +6,7 @@ import pet_hotel_2 from "../../../../assets/png/pet_hotel_2.png";
 
 const LostAdvertising = () => {
     return (
-        <div className={`${style.mainGreenBack} d-flex flex-column`}>
+        <div className={`d-flex flex-column`}>
             <img className={`${style.dental_care} ms-3 mt-3`} src={dental_care} alt={'dental_care'}/>
             <img className={`${style.pet_hotel} ms-3 mt-3`} src={pet_hotel} alt={'pet_hotel'}/>
             <img className={`${style.pet_hotel_2} ms-3 mt-3`} src={pet_hotel_2} alt={'pet_hotel_2'}/>
--- src/components/pages/start_page/Start_four/Start_four.jsx
+++ src/components/pages/start_page/Start_four/Start_four.jsx
@@ -4,22 +4,19 @@ import style from './start_four.module.css';
 
 const StartFour = () => {
     return (
-        <section className={`${style.colorSection}`}>
-            <div className={`row justify-content-center align-items-center pb-2 pt-2`}>
-                <div className={'d-flex col-10 align-items-center'}>
-                    <div className={`col-6`}>
-                        <p className={`${style.aboutStyle}`}>Coming soon</p>
-                    </div>
-                    <div className={`row align-items-center col-6 justify-content-between`}>
-                        <div className={`${style.widthInfo}`}>
-                            <p className={`${style.textStyle}`}>We are planing to open a new service,<br/> where your
-                                cats and dogs can find their love!</p>
-                        </div>
-                        <div className={`${style.logoLove} col-3`}>
-                            <img src={love} alt="love"/>
-                            <p className={`${style.pLove}`}>LOVE</p>
-                        </div>
-                    </div>
+        <section className={style.section}>
+            <div className={`${style.colorSection}`}>
+                <div>
+                    <p className={`${style.aboutStyle}`}>Coming soon</p>
+                </div>
+
+                <div className={style.openService}>
+                    <p className={`${style.textStyle}`}>We are planing to open a new service,<br/> where your
+                        cats and dogs can find their love!</p>
+                </div>
+                <div className={`${style.logoLove} col-3`}>
+                    <img src={love} alt="love"/>
+                    <p className={`${style.pLove}`}>LOVE</p>
                 </div>
             </div>
         </section>
@@ -27,27 +24,3 @@ const StartFour = () => {
 };
 
 export default StartFour;
-
-// const StartFour = () => {
-//     return (
-//         <section className={`${style.colorSection} container-fluid`}>
-//             <div className={`row justify-content-center align-items-center pb-2 pt-2`}>
-//                 <div className={'d-flex col-10 align-items-center'}>
-//                     <div className={`col-6`}>
-//                         <p className={`${style.aboutStyle}`}>Coming soon</p>
-//                     </div>
-//                     <div className={`row align-items-center col-6 justify-content-between`}>
-//                         <div className={`${style.widthInfo}`}>
-//                             <p className={`${style.textStyle}`}>We are planing to open a new service,<br/> where your
-//                                 cats and dogs can find their love!</p>
-//                         </div>
-//                         <div className={`${style.logoLove} col-3`}>
-//                             <img src={love} alt="love"/>
-//                             <p className={`${style.pLove}`}>LOVE</p>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// };
