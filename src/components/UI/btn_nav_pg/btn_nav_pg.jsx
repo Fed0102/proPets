95b891b34c2772badfab15f9751c5d5f9a7bdf6a
irklva <irk.lva@gmail.com>
2023-03-27 12:17:42 +0300

    btn_st_change_start

--- src/components/UI/btn_nav_pg/btn_nav_pg.jsx
+++ src/components/UI/btn_nav_pg/btn_nav_pg.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import st from './btn_nav_pg.module.css'
 import {Link} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {SET_CHANGE_MAIN_BODY} from "../../../store/modalReducer";
 
 const BtnNavPg = ({children, btnPath}) => {
-
-    const dispatch = useDispatch();
-
     return (
-        <div className={st.base}>
+        <div className={{st.base}}>
             <Link to={btnPath} className={st.link}>
-                <div className={st.normal} onClick={()=>dispatch({type:SET_CHANGE_MAIN_BODY, payload: 'HomePosts'})}>
+                <div className={st.normal}>
                     {children}
                 </div>
             </Link>
