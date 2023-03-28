1d1e00b0028efe3be50ad2826e0430bda9d3be71
Fed0102 <127963492+Fed0102@users.noreply.github.com>
2023-03-28 15:06:48 +0300

    auth2->auth

--- src/firebase/auth-service.js
+++ src/firebase/auth-service.js
@@ -1,20 +1,20 @@
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth';
-import {auth2} from "./firebase-config";
+import {auth} from "./firebase-config";
 
 export function registration(email, password) {
-    createUserWithEmailAndPassword(auth2, email, password)
+    createUserWithEmailAndPassword(auth, email, password)
         .then(response => console.log(response))
         .catch(e => console.log(e))
 }
 
 export function login(email, password) {
-    signInWithEmailAndPassword(auth2, email, password)
+    signInWithEmailAndPassword(auth, email, password)
         .then(response => console.log(response))
         .catch(e => console.log(e))
 }
 
 export function logout() {
-    signOut(auth2)
+    signOut(auth)
         .then(response => console.log(response))
         .catch(e => console.log(e))
 }
--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -12,4 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth2 = getAuth(app);
+export const auth = getAuth(app);
