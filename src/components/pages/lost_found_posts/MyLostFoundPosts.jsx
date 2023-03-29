8eeb98e942c13edd9c346885f4d8216c7d4f887f
Fed0102 <127963492+Fed0102@users.noreply.github.com>
2023-03-29 23:29:09 +0300

    ok

--- src/components/pages/lost_found_posts/MyLostFoundPosts.jsx
+++ src/components/pages/lost_found_posts/MyLostFoundPosts.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import LostInputs from "../../UI/lost_found_inputs/lost_found_inputs";
-import LostCard from "../../UI/lost_found_card/lost_found_card";
+// import LostCard from "../../UI/lost_found_card/lost_found_card";
 import style from '../../UI/lost_found_inputs/lost_found_inputs.module.css';
 
 const Lost = () => {
@@ -8,7 +8,7 @@ const Lost = () => {
         <div className={`${style.myLostFoundPostsDiv} justify-content-center align-content-center d-flex`}>
             <div>
                 <LostInputs/>
-                <LostCard/>
+                {/*<LostCard/>*/}
             </div>
         </div>
     );
