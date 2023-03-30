9b15c0474c3a65f1911b0c0a34b9de33308a99de
Fed0102 <127963492+Fed0102@users.noreply.github.com>
2023-03-30 15:36:44 +0300

    MyPostForm

--- src/firebase/auth-service.js
+++ src/firebase/auth-service.js
@@ -1,4 +1,4 @@
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth';
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
 import {auth} from "./firebase-config";
 
 
