886546104c5ac472d81503a0a4c8f20d7a1d1456
Fed0102 <127963492+Fed0102@users.noreply.github.com>
2023-04-02 13:23:55 +0300

    Map

--- src/components/main_body/right_block/MyBlock.jsx
+++ src/components/main_body/right_block/MyBlock.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import st from './block.module.css';
-// import MyMap from "./map/MyMap";
+import MyMap from "./map/MyMap";
+import {useWindowPath} from "../../../hooks/useWindowPath";
+import {favorites, fostering, found, homePage, hotels, lost, vetHelp, walking} from "../../../router/paths";
 import MyAdvertising from "./advertising/MyAdvertising";
+// import MyAdvertising from "./advertising/MyAdvertising";
 
 
 const MyBlock = () => {
+
+    const path = useWindowPath();
+
+    const mapOrAdvice = () => {
+        if (path === lost || path === found)
+            return <MyMap/>
+        else if (path === homePage || path === hotels || path === walking || path === fostering || path === vetHelp ||path === favorites)
+            return <MyAdvertising/>
+    }
+
     return (
         <div className={st.base}>
-            {/*<MyMap/>*/}
-            <MyAdvertising/>
+            {mapOrAdvice()}
         </div>
     );
 };
