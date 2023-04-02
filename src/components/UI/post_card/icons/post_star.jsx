e7e4eb74da8991134da470d9845dd53043d9fcea
Fed0102 <127963492+Fed0102@users.noreply.github.com>
2023-04-03 00:01:14 +0300

    star

--- src/components/UI/post_card/icons/post_star.jsx
+++ src/components/UI/post_card/icons/post_star.jsx
@@ -1,29 +1,12 @@
 import React from 'react';
 import star from '../../../../assets/png/star_empty.png';
-// import test from '../../../../assets/svg/star_test.svg'
 import st from './post_star.module.css'
 import {Link} from "react-router-dom";
 import {favorites} from "../../../../router/paths";
-// import {addDoc, collection} from "firebase/firestore";
-// import {db} from "../../../../firebase/firebase-config";
 
 const PostStar = () => {
 
-    //
-    // const [favoritesPost, setFavoritesPost] = useState();
-    //
-    // const addBase = () => {
-    //     try {
-    //         addDoc(collection(db, "post"), {
-    //             favoritesPost: favoritesPost,
-    //         }).then(r => console.log(r));
-    //     } catch (e) {
-    //         console.error("Error adding document: ", e);
-    //     }
-    // }
-
     return (
-        // <Link to={favorites} setPost={setFavoritesPost} onClick={()=>addBase()}>
         <Link to={favorites} className={st.star}>
             <img className={st.body} src={star} alt={'star'}/>
         </Link>
